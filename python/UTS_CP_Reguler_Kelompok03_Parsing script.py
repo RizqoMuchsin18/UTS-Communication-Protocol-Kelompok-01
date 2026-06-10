64e3421fa6f7f8b84a7eddc647e642dc3d52a7e0
RizqoMuchsin18 <rizkomuchsin@gmail.com>
2026-06-10 15:30:35 +0700

    Add files via upload

---
+++ python/UTS_CP_Reguler_Kelompok03_Parsing script.py
@@ -0,0 +1,13 @@
+import json
+import pandas as pd
+
+with open("users.json", "r", encoding="utf-8") as f:
+    data = json.load(f)
+
+df = pd.DataFrame(data)
+
+print(df[["id", "name", "username", "email"]])
+
+df.to_csv("users.csv", index=False)
+
+print("File users.csv berhasil dibuat")
